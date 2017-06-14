80937f81f4d443f2ae66509e759de883b843a0ed
jasmin <jasujnasit48@gmail.com>
2017-06-14 19:43:18 +0530

    login page is ready

---
+++ html/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<div class="mainback">
+
+		<div class="mainblack">
+
+			<div class="row1">
+				<div class="col1">
+					<img class="logo" src="../images/logo.png">
+				</div>
+
+			</div>
+			<div class="row2">
+				<div class="col2">
+					<div class="row21">
+						Log In To Center4Info		
+					</div>
+					<div class="row22">
+					  <form name="loginform" action="login.php">
+					  	<table class="logintab" >
+					  	<tr>
+					  		<td>USER ID:</td><td><input type="text" name="userid"></td>
+					  	</tr>
+					  	<tr>
+					  		<td>PASSWORD:</td><td><input type="password" name="psswd"></td>
+					  	</tr>
+					  	<tr>
+					  		<td colspan="2"><input class="logbtn" type="submit" name="loginbtn" value="Log In"></td>
+					  	</tr>
+					  	<tr><td text-align="right" colspan="2">
+					  	    New User?
+					  	</td></tr>
+					  	</table>
+					  </form>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</body>
+	</html>

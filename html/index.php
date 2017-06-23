08edaf9f9e4382fdaa1a490dd3b7b6a9eda65314
jasmin <jasujnasit48@gmail.com>
2017-06-23 12:56:43 +0530

    login session is created

--- html/index.php
+++ html/index.php
@@ -1,10 +1,35 @@
+<?php
+$name="";
+$usid="";
+if(!isset($_COOKIE['user'])){
+   header("location:login.php");
+}
+else{
+$usid=$_COOKIE["user"];
+$data=mysqli_connect("localhost","root","","Center4Info") or die();
+$db=mysqli_query($data,"SELECT `fname`,`lname` FROM login WHERE `userid`='$usid'");
+$db=mysqli_fetch_assoc($db);
+$fn=$db['fname'];
+$ln=$db['lname'];
+$name=$fn." ".$ln;
+}
+
+?>
+
+
+
+
 <!DOCTYPE HTML>
-<html><head><title>black &amp; white</title><meta name="description" content="website description"><meta name="keywords" content="website keywords, website keywords"><meta http-equiv="content-type" content="text/html; charset=windows-1252"><link rel="stylesheet" type="text/css" href="../css/style.css" title="style"></head><body>
+<html><head><title>black &amp; white</title><meta name="description" content="website description"><meta name="keywords" content="website keywords, website keywords"><meta http-equiv="content-type" content="text/html; charset=windows-1252"><link rel="stylesheet" type="text/css" href="../css/style.css?version=51" title="style"><link rel="stylesheet" type="text/css" href="../css/searchengine.css?version=51" title="style"></head><body>
   <div id="main">
     <div id="header">
       <div id="logo">
         <div class="black">
         <img class="clogo" src="../images/logo.png">
+        <div class="loginname">
+        <?php echo $name; ?><br>
+        <?php echo $usid; ?>
+        </div>
       </div>
       </div>
       <div id="menubar">
@@ -15,7 +40,18 @@
           <li><a href="about.php">About Us</a></li>
         </ul></div>
     </div>
-   
+   <div class="maincontent">
+        <div class="lbar">
+          
+        </div>
+        <div class="rbar">
+        <div class="searchbar">
+          <input type="searchengine" name="sengine" placeholder="Search..." id="sbar">
+          <button id="sbtn">Search</button>
+        </div>
+        </div>
+
+   </div>
     <div id="content_footer"></div>
     <div id="footer">
      
--- html/signup.php
+++ html/signup.php
@@ -1,25 +1,33 @@
 <?php
+$result="";
+
 if(isset($_GET["signupbtn"]))
 {
-$finame=$_GET["fname"];
-$liname=$_GET["lname"];
-$mail=$_GET["emailid"];
-$uid=$_GET["userid"];
-$passwd=$_GET["psswd"];
+	$flag=0;
+	$finame=$_GET["fname"];
+	$liname=$_GET["lname"];
+	$mail=$_GET["emailid"];
+	$uid=$_GET["userid"];
+	$passwd=$_GET["psswd"];
 
-$db=mysqli_connect("localhost","root","","Center4Info") or die();
-$db=mysqli_query($db,"SELECT `userid` FROM login");
-$db=mysqli_fetch_assoc($db);
-foreach ($db as $id) {
-	if($id==$uid)
-	{
-		$message = "Userid Already Registered";
-        echo "<script type='text/javascript'>alert('$message');</script>";
+	$data=mysqli_connect("localhost","root","","Center4Info") or die();
+	$db=mysqli_query($data,"SELECT `userid` FROM login");
+	foreach ($db as $id) {
+		$a=$id['userid'];
+		if($a==$uid)
+		{
+		 	 $flag=1;
+             break;
+		}
 	}
-}
-mysqli_query($db,"INSERT INTO login VALUES('$uid','$passwd','$finame','$liname','$mail')");
-setcookie('signup','1',time()+60);
-header("location:login.php");
+	if($flag==1){
+		$result="User id Is Already Registered!!";
+	}
+	if($flag==0){
+		mysqli_query($data,"INSERT INTO login VALUES('$uid','$passwd','$finame','$liname','$mail')");
+		setcookie('signup','1',time()+60);
+		header("location:login.php");
+	}
 
 }
 
@@ -30,7 +38,7 @@ header("location:login.php");
 <head>
 	<title>signup</title>
 	<link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css?version=1">
 </head>
 <body>
 	<div class="mainback">
@@ -46,33 +54,34 @@ header("location:login.php");
 			<div class="row2" id="row2">
 				<div class="col2" id="col2">
 					<div class="row21" >
-						Sign Up 		
+						Sign Up <br>
+						<div class="res"><?php echo $result; ?></div>		
 					</div>
 					<div class="row22" >
-					  <form name="signupform" action="signup.php" method="get">
-					  	<table class="logintab" >
-					  	
-					  	<tr>
-					  		<td>FIRST NAME:</td><td><input type="text" name="fname"></td>
-					  	</tr>
-					  	<tr>
-					  		<td>LAST NAME:</td><td><input type="text" name="lname"></td>
-					  	</tr>
-					  	<tr>
-					  		<td>EMAIL ID:</td><td><input type="text" name="emailid"></td>
-					  	</tr>
-					  	<tr>
-					  		<td>USER ID:</td><td><input type="text" name="userid"></td>
-					  	</tr>
-					  	<tr>
-					  		<td>PASSWORD:</td><td><input type="password" name="psswd"></td>
-					  	</tr>
-					  	<tr>
-					  		<td colspan="2"><input class="logbtn" type="submit" name="signupbtn" value="Sign Up"></td>
-					  	</tr>
-					  
-					  	</table>
-					  </form>
+						<form name="signupform" action="signup.php" method="get">
+							<table class="logintab" >
+
+								<tr>
+									<td>FIRST NAME:</td><td><input type="text" name="fname"></td>
+								</tr>
+								<tr>
+									<td>LAST NAME:</td><td><input type="text" name="lname"></td>
+								</tr>
+								<tr>
+									<td>EMAIL ID:</td><td><input type="text" name="emailid"></td>
+								</tr>
+								<tr>
+									<td>USER ID:</td><td><input type="text" name="userid"></td>
+								</tr>
+								<tr>
+									<td>PASSWORD:</td><td><input type="password" name="psswd"></td>
+								</tr>
+								<tr>
+									<td colspan="2"><input class="logbtn" type="submit" name="signupbtn" value="Sign Up"></td>
+								</tr>
+
+							</table>
+						</form>
 					</div>
 				</div>
 			</div>

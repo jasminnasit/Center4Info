733cf45e86f0063f6c270fba093fecebb657424c
jasmin <jasujnasit48@gmail.com>
2017-07-11 09:41:52 +0530

    jquery fixed

--- html/about.php
+++ html/about.php
@@ -18,7 +18,7 @@ else{
 ?>
 
 <!DOCTYPE HTML>
-<html><head><title>black &amp; white</title><meta name="description" content="website description"><meta name="keywords" content="website keywords, website keywords"><meta http-equiv="content-type" content="text/html; charset=windows-1252"><link rel="stylesheet" type="text/css" href="../css/style.css" title="style"></head><body>
+<html><head><title>black &amp; white</title><meta name="description" content="website description"><meta name="keywords" content="website keywords, website keywords"><meta http-equiv="content-type" content="text/html; charset=windows-1252"><link rel="stylesheet" type="text/css" href="../css/style.css?version=51" title="style"></head><body>
   <div id="main">
     <div id="header">
       <div id="logo">
@@ -31,14 +31,55 @@ else{
       </div>
       </div>
       <div id="menubar">
-        <ul id="menu"><!-- put class="selected" in the li tag for the selected page - to highlight which page you're on --><li ><a href="index.php">Home</a></li>
+        <ul id="menu"> <li ><a href="index.php">Home</a></li>
           <li><a href="collegeevents.php">College Events</a></li>
           <li><a href="exams.php">Exams</a></li>
           <li><a href="curriculum.php">Curriculum</a></li>
           <li class="selected"><a href="about.php">About Us</a></li>
         </ul></div>
     </div>
-  
+  	 <div class="currimain">
+  	 <div class="webhead">
+    	 	-: Web Developers :-
+    	 </div>
+    	 <div class="curriframe">
+    	 
+         <div class="intro">
+         	<div class="photo">
+         		<img src="../images/tarzen.jpg" class="photos">
+         	</div>
+         	<div class="name">
+         		JASMIN NASIT
+         	</div>
+         </div>
+         <div class="intro">
+             <div class="photo">
+         		<img src="../images/jay.jpg" class="photos">
+         	</div>
+         	<div class="name">
+         		JAY DUDHAT
+         	</div>
+         </div>
+         <div class="intro">
+         	<div class="photo">
+         		<img src="../images/priyansh.jpg" class="photos">
+         	</div>
+         	<div class="name">
+         		PRIYANSH ZALAVADIYA
+         	</div>
+         </div>
+         
+
+    	 </div>
+    	 <div class="contact">
+    				<div>-:Contact us:-</div>
+    					<div>
+    						<h3>Email : center4info@gmail.com</h3>
+    					</div>    		 			 
+         </div>
+
+    </div>
+    
     <div id="content_footer"></div>
     <div id="footer">
      
--- html/curriculum.php
+++ html/curriculum.php
@@ -18,7 +18,7 @@ else{
 ?>
 
 <!DOCTYPE HTML>
-<html><head><title>black &amp; white</title><meta name="description" content="website description"><meta name="keywords" content="website keywords, website keywords"><meta http-equiv="content-type" content="text/html; charset=windows-1252"><link rel="stylesheet" type="text/css" href="../css/style.css" title="style"></head><body>
+<html><head><title>black &amp; white</title><meta name="description" content="website description"><meta name="keywords" content="website keywords, website keywords"><meta http-equiv="content-type" content="text/html; charset=windows-1252"><link rel="stylesheet" type="text/css" href="../css/style.css?version=51" title="style"></head><body>
   <div id="main">
     <div id="header">
       <div id="logo">
@@ -31,12 +31,26 @@ else{
       </div>
       </div>
       <div id="menubar">
-        <ul id="menu"><!-- put class="selected" in the li tag for the selected page - to highlight which page you're on --><li ><a href="index.php">Home</a></li>
+        <ul id="menu"> <li ><a href="index.php">Home</a></li>
           <li><a href="collegeevents.php">College Events</a></li>
           <li><a href="exams.php">Exams</a></li>
           <li class="selected"><a href="curriculum.php">Curriculum</a></li>
           <li><a href="about.php">About Us</a></li>
         </ul></div>
+    </div>
+    <div class="currimain">
+    	<div class="curriframe">
+    		<div class="lside">
+    			<a href="http://www.svnit.ac.in/web/department/computer/btech_1.php" target="aaaa" ><div class="lleft">1<sup>st</sup>YEAR</div></a>
+    			<div class="lleft">2<sup>nd</sup>YEAR</div>
+    			<div class="lleft">3<sup>rd</sup>YEAR</div>
+    			<div class="lleft">4<sup>th</sup>YEAR</div>
+    		</div>
+    		<div class="rside">
+    			<iframe name="aaaa" class="rside2"></iframe>
+    		</div>
+    	</div>
+
     </div>
 
     <div id="content_footer"></div>
